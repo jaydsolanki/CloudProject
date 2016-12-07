fe869deaf1532c8b2bf3c429eb38377fd1b37360
jaydsolanki <jayd_solanki@yahoo.com>
2016-12-07 15:43:06 -0500

    Added script to convert Sqlite3 db to json for further use in elastic search

---
+++ CloudProject/db_to_json.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def db_to_json(file_name, output_file_name):
+    conn = sqlite3.connect(file_name)
+    cursor = conn.cursor()
+    cursor.execute("select * from parking_data")
+    records = cursor.fetchall()
+    f = open(output_file_name,'w')
+    for record in records:
+        parking_allowed = 'true' if record[6]==1 else 'false'
+        json_record = '{"latitude":'+str(record[1])+', "longitude":'+str(record[2])+', "parking_spots":'+str(record[3])+', "street_ave_name":"'+str(record[4])+'", "between_street_ave":"'+str(record[5])+'", "parking_allowed":'+parking_allowed+', "parking_on":"'+record[7]+'"}'
+        f.write(json_record+"\n")
+    f.close()
+    cursor.close()
+    conn.close()
+
+db_to_json('db.sqlite3','parking_data.json')
